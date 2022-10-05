b9469d35b69cab65a4ed5f1d556bb941353349f5
João Carlos <joaoc.avils@gmail.com>
2022-10-05 10:11:48 -0300

    feat: adicionado comandos de cli com cobra

--- main.go
+++ main.go
@@ -1,19 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"database/sql"
-
-	"github.com/joaocsv/hexagonal-example/adapters/db"
-	"github.com/joaocsv/hexagonal-example/app"
-	_ "github.com/mattn/go-sqlite3"
-)
+import "github.com/joaocsv/hexagonal-example/cmd"
 
 func main() {
-	connection, _ := sql.Open("sqlite3", "db.sqlite")
-	productDb := db.NewProductDb(connection)
-	productService := app.NewProductService(productDb)
-
-	product, _ := productService.Create("Coca-cola 2L", 8.30)
-
-	productService.Enable(product)
+	cmd.Execute()
 }
